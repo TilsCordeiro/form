07f8cf5e30ec91164256b2c07d16e73c21fc32d0
TilsCordeiro <tilscordeiro@gmail.com>
2025-03-29 17:00:51 -0300

    Configuração inicial
    Configuração inicial usando FastAPI

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Form #Capturar dados enviados
+from fastapi.responses import HTMLResponse #Retornar páginas HTML
+from fastapi.templating import Jinja2Templates #Usar templates HTML dinâmicos
+from starlette.requests import Request #Requisição do usuário
+
+app = FastAPI() #inicia
+templates = Jinja2Templates(directory="templates") #define que os arquivos HTML estarão na pasta templates
+
+@app.get("/", response_class=HTMLResponse)
+async def form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "media": None})
+
+@app.post("/calcular")
+async def calcular_media(request: Request, nota1: float = Form(...), nota2: float = Form(...)):
+    media = (nota1 + nota2) / 2
+    return templates.TemplateResponse("index.html", {"request": request, "media": media})
